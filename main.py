cf2eb5a62479b3f9151d5673c7570f545a59045b
FabricioNascimentop <119469677+FabricioNascimentop@users.noreply.github.com>
2023-07-19 02:07:39 -0300

    inicio

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, redirect, session
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.secret_key = 'fabricio'
+app.config['SQLACHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root:''@localhost/filmesteca"
 #criação da classe "filme", modo primitivo de armazenar dados da lista
 class filme:
     def __init__(self,nome, genero, diretor,ano):
@@ -9,7 +11,7 @@ class filme:
         self.diretor = diretor
         self.ano = ano
 #exemplos iniciais pra enriquecer visualmente a experiência de quem entra no site
-avatar = filme('avatar 2: caminho do n sei oq', 'aventura/fantasia','james cameron',2022)
+avatar = filme('avatar 2: caminho', 'aventura/fantasia','james cameron',2022)
 joker = filme('joker','drama','todd philips',2019)
 jack = filme('o estranho mundo de jack','animação','tim burton',1993)
 ldj = filme('liga da justiça','ação','jack snider',2021)
@@ -43,12 +45,13 @@ def verificarro(lista,novo_filme):
 
 @app.route('/catalogar',methods=['POST',])
 def catalogar():
-    nome = request.form['novo_filme']
-    genero = request.form['genero']
-    diretor = request.form['diretor']
-    var_filme = filme(nome, genero, diretor)
+    nome = request.form.get('nome_cadastro')
+    genero = request.form.get('genero_cadastro')
+    diretor = request.form.get('diretor_cadastro')
+    ano = request.form.get('ano_cadastro')
+    var_filme = filme(nome, genero, diretor, ano)
     if verificarro(lista, var_filme) == False:
-        lista.append(filme(nome, genero, diretor))
+        lista.append(filme(nome, genero, diretor, ano))
     
     return render_template('index.html',lista=lista)
 
